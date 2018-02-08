6038c17864792261b3bf8699730f1d51e612066b
Florent Clairambault <florent@clairambault.fr>
2018-02-08 01:43:45 +0100

    Changes by @pterr

---
+++ calypso/src/main/java/org/keyple/commands/CommandType.java
@@ -0,0 +1,14 @@
+package org.keyple.calypso.commands;
+
+/**
+ * This enumeration registers the types of the commands .
+ *
+ * @author Ixxi
+ */
+public enum CommandType {
+
+    /** The po. */
+    PO,
+    /** The csm. */
+    CSM;
+}
---
+++ calypso/src/main/java/org/keyple/commands/csm/parser/DigestAuthenticateRespPars.java
@@ -0,0 +1,33 @@
+package org.keyple.calypso.commands.csm.parser;
+
+import org.keyple.commands.ApduResponseParser;
+import org.keyple.seproxy.ApduResponse;
+
+/**
+ * This class provides the dedicated constructor to parse the Digest
+ * Authenticate response.
+ *
+ * @author Ixxi
+ *
+ */
+public class DigestAuthenticateRespPars extends ApduResponseParser {
+
+    /**
+     * Instantiates a new DigestAuthenticateRespPars.
+     *
+     * @param response
+     *            from the CSM DigestAuthenticateCmdBuild
+     */
+    public DigestAuthenticateRespPars(ApduResponse response) {
+        super(response);
+        initStatusTable();
+    }
+
+    /**
+     * Initializes the status table.
+     */
+    private void initStatusTable() {
+        statusTable.put(new byte[] { (byte) 0x90, (byte) 0x00 }, new StatusProperties(true, "Successful execution."));
+    }
+
+}
