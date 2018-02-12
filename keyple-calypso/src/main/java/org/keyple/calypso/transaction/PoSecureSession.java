c9c7b90fb71cdbb6f728de30e80f13c55656ca57
Florent Clairambault <florent@clairambault.fr>
2018-02-12 21:58:30 +0100

    Updating the license

--- keyple-calypso/src/main/java/org/keyple/calypso/transaction/PoSecureSession.java
+++ keyple-calypso/src/main/java/org/keyple/calypso/transaction/PoSecureSession.java
@@ -1,7 +1,9 @@
 /*
- * Copyright 2018 Keyple - https://keyple.org/
+ * Copyright (c) 2018 Calypso Networks Association https://www.calypsonet-asso.org/
  *
- * Licensed under GPL/MIT/Apache ???
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License version 2.0 which accompanies this distribution, and is
+ * available at https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
  */
 
 package org.keyple.calypso.transaction;
