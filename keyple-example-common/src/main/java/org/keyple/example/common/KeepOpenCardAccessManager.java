e48a9659266111e1e5faf12ab8e8b32fb0ce6c76
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2018-05-16 18:23:08 +0200

    Fix issue 103: factoring of common demo code

--- keyple-example-common/src/main/java/org/keyple/example/common/KeepOpenCardAccessManager.java
+++ keyple-example-common/src/main/java/org/keyple/example/common/KeepOpenCardAccessManager.java
@@ -8,20 +8,13 @@
 
 package org.keyple.example.common;
 
-import java.util.Arrays;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
-import org.keyple.calypso.commands.po.PoRevision;
-import org.keyple.calypso.commands.po.builder.ReadRecordsCmdBuild;
-import org.keyple.calypso.commands.po.builder.UpdateRecordCmdBuild;
-import org.keyple.seproxy.ApduRequest;
-import org.keyple.seproxy.ByteBufferUtils;
-import org.keyple.seproxy.ProxyReader;
-import org.keyple.seproxy.SeRequest;
-import org.keyple.seproxy.SeResponse;
+import org.keyple.seproxy.*;
 
 
 public class KeepOpenCardAccessManager extends AbstractLogicManager {
-
 
     private ProxyReader poReader;
 
@@ -32,49 +25,51 @@ public class KeepOpenCardAccessManager extends AbstractLogicManager {
     @Override
     public void run() {
         super.run();
-        String poAid = "A000000291A000000191";
-        String t2UsageRecord1_dataFill = "0102030405060708090A0B0C0D0E0F10"
-                + "1112131415161718191A1B1C1D1E1F20" + "2122232425262728292A2B2C2D2E2F30";
 
-        ReadRecordsCmdBuild poReadRecordCmd_T2Env = new ReadRecordsCmdBuild(PoRevision.REV3_1,
-                (byte) 0x01, true, (byte) 0x14, (byte) 0x20);
-        ReadRecordsCmdBuild poReadRecordCmd_T2Usage = new ReadRecordsCmdBuild(PoRevision.REV3_1,
-                (byte) 0x01, true, (byte) 0x1A, (byte) 0x30);
-        UpdateRecordCmdBuild poUpdateRecordCmd_T2UsageFill =
-                new UpdateRecordCmdBuild(PoRevision.REV3_1, (byte) 0x01, (byte) 0x1A,
-                        ByteBufferUtils.fromHex(t2UsageRecord1_dataFill));
+        // Hoplink simple read requests builder object
+        HoplinkSimpleRead hoplinkSimpleRead = new HoplinkSimpleRead();
 
-        // Get PO ApduRequest List
-        List<ApduRequest> poApduRequestList = Arrays.asList(poReadRecordCmd_T2Env.getApduRequest(),
-                poReadRecordCmd_T2Usage.getApduRequest(),
-                poUpdateRecordCmd_T2UsageFill.getApduRequest());
+        ByteBuffer poAid = hoplinkSimpleRead.getPoAid();
 
-        SeRequest poRequest =
-                new SeRequest(ByteBufferUtils.fromHex(poAid), poApduRequestList, true);
+        List<ApduRequest> poApduRequestList = hoplinkSimpleRead.getPoApduRequestList();
+
+
+        // Create request elements list
+        List<SeRequestElement> poRequestElements = new ArrayList<SeRequestElement>();
+
+        // Add Aid and apdu requests from Hoplink builder
+        poRequestElements.add(new SeRequestElement(poAid, poApduRequestList, true));
+
+        // Create SeRequest
+        SeRequest poRequest = new SeRequest(poRequestElements);
+
         try {
 
             System.out.println("Transmit 1st SE Request, keep channel open");
             SeResponse poResponse = poReader.transmit(poRequest);
-            getTopic()
-                    .post(new Event("Got a response", "poResponse", poResponse.getApduResponses()));
+            SeResponseElement poResponseElement = poResponse.getElements().get(0);
+            getTopic().post(new Event("Got a response", "poResponse",
+                    poResponseElement.getApduResponses()));
 
             System.out.println("Sleeping for 3 seconds");
             Thread.sleep(3000);
             System.out.println("Transmit 2nd SE Request, close channel");
 
-            SeRequest poRequest2 =
-                    new SeRequest(ByteBufferUtils.fromHex(poAid), poApduRequestList, false);
+            // Create 2nd request elements list
+            List<SeRequestElement> poRequestElements2 = new ArrayList<SeRequestElement>();
+
+            // Add same Aid and apdu requests from Hoplink builder
+            poRequestElements2.add(new SeRequestElement(poAid, poApduRequestList, false));
+
+            // Create SeRequest
+            SeRequest poRequest2 = new SeRequest(poRequestElements);
 
             SeResponse poResponse2 = poReader.transmit(poRequest2);
-            getTopic().post(
-                    new Event("Got a 2nd response", "poResponse2", poResponse2.getApduResponses()));
-
-
+            SeResponseElement poResponseElement2 = poResponse2.getElements().get(0);
+            getTopic().post(new Event("Got a 2nd response", "poResponse2",
+                    poResponseElement2.getApduResponses()));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-
-
 }
