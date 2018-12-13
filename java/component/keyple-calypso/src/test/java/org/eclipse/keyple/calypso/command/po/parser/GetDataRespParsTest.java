c24540f66e33d810b3e372806343c8d578b6d1ee
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2018-12-13 18:12:24 +0100

    Updated and improved the coverage of unit tests of the FCI parser

--- java/component/keyple-calypso/src/test/java/org/eclipse/keyple/calypso/command/po/parser/GetDataRespParsTest.java
+++ java/component/keyple-calypso/src/test/java/org/eclipse/keyple/calypso/command/po/parser/GetDataRespParsTest.java
@@ -11,13 +11,8 @@
  ********************************************************************************/
 package org.eclipse.keyple.calypso.command.po.parser;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.eclipse.keyple.command.AbstractApduResponseParser;
+
 import org.eclipse.keyple.seproxy.message.ApduResponse;
-import org.eclipse.keyple.seproxy.message.SeResponse;
-import org.eclipse.keyple.seproxy.message.SeResponseSet;
-import org.eclipse.keyple.seproxy.message.SelectionStatus;
 import org.eclipse.keyple.util.ByteArrayUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -28,19 +23,209 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class GetDataRespParsTest {
 
     @Test
-    public void digestInitRespPars() {
-        List<ApduResponse> responses = new ArrayList<ApduResponse>();
-        ApduResponse apduResponse = new ApduResponse(new byte[] {(byte) 0x90, 0x00}, null);
-        responses.add(apduResponse);
-        SeResponseSet seResponse =
-                new SeResponseSet(new SeResponse(true,
-                        new SelectionStatus(null,
-                                new ApduResponse(ByteArrayUtils.fromHex("9000"), null), true),
-                        responses));
-        AbstractApduResponseParser apduResponseParser =
-                new GetDataFciRespPars(seResponse.getSingleResponse().getApduResponses().get(0));
+    public void testFCIparser_FCI_OK() {
+        String dfName = "00112233445566778899";
+        String appSerialNumber = "AABBCCDDEEFF0011";
+        byte siBufferSizeIndicator = 11;
+        byte siPlatform = (byte) 0x55;
+        byte siApplicationType = (byte) 0xAA;
+        byte siApplicationSubtype = (byte) 0x55;
+        byte siSoftwareIssuer = (byte) 0xAA;
+        byte siSoftwareVersion = (byte) 0x55;
+        byte siSoftwareRevision = (byte) 0xAA;
+        String startupInformation = String.format("%02X%02X%02X%02X%02X%02X%02X",
+                siBufferSizeIndicator, siPlatform, siApplicationType, siApplicationSubtype,
+                siSoftwareIssuer, siSoftwareVersion, siSoftwareRevision);
+        ApduResponse apduResponse = new ApduResponse(
+                ByteArrayUtils.fromHex("6F 24 84 0A " + dfName + " A5 16 BF0C 13 C7 08 "
+                        + appSerialNumber + " 53 07 " + startupInformation + "9000"),
+                null);
 
-        byte[] responseActual = apduResponseParser.getApduResponse().getBytes();
-        Assert.assertArrayEquals(new byte[] {(byte) 0x90, 0x00}, responseActual);
+        GetDataFciRespPars parser = new GetDataFciRespPars(apduResponse);
+
+        /* valid Calypso FCI */
+        Assert.assertTrue(parser.isValidCalypsoFCI());
+        /* DF not invalidated */
+        Assert.assertFalse(parser.isDfInvalidated());
+        /* expected dfName */
+        Assert.assertArrayEquals(ByteArrayUtils.fromHex(dfName), parser.getDfName());
+        /* expected Application Serial Number */
+        Assert.assertArrayEquals(ByteArrayUtils.fromHex(appSerialNumber),
+                parser.getApplicationSerialNumber());
+        /* Buffer size indicator and value */
+        Assert.assertEquals(siBufferSizeIndicator, parser.getBufferSizeIndicator());
+        Assert.assertEquals(512, parser.getBufferSizeValue());
+        /* Platform */
+        Assert.assertEquals(siPlatform, parser.getPlatformByte());
+        /* ApplicationType */
+        Assert.assertEquals(siApplicationType, parser.getApplicationTypeByte());
+        Assert.assertTrue(parser.isRev3_2ModeAvailable());
+        Assert.assertTrue(parser.isRatificationCommandRequired());
+        Assert.assertTrue(parser.hasCalypsoPin());
+        Assert.assertFalse(parser.hasCalypsoStoredValue());
+        /* ApplicationSubType */
+        Assert.assertEquals(siApplicationSubtype, parser.getApplicationSubtypeByte());
+        /* SoftwareIssuer */
+        Assert.assertEquals(siSoftwareIssuer, parser.getSoftwareIssuerByte());
+        /* SoftwareVersion */
+        Assert.assertEquals(siSoftwareVersion, parser.getSoftwareVersionByte());
+        /* SoftwareRevision */
+        Assert.assertEquals(siSoftwareRevision, parser.getSoftwareRevisionByte());
+
+        /* Change startup information */
+        siBufferSizeIndicator = 16;
+        siPlatform = (byte) 0xAA;
+        siApplicationType = (byte) 0x55;
+        siApplicationSubtype = (byte) 0xAA;
+        siSoftwareIssuer = (byte) 0x55;
+        siSoftwareVersion = (byte) 0xAA;
+        siSoftwareRevision = (byte) 0x55;
+        startupInformation = String.format("%02X%02X%02X%02X%02X%02X%02X", siBufferSizeIndicator,
+                siPlatform, siApplicationType, siApplicationSubtype, siSoftwareIssuer,
+                siSoftwareVersion, siSoftwareRevision);
+        apduResponse = new ApduResponse(
+                ByteArrayUtils.fromHex("6F 24 84 0A " + dfName + " A5 16 BF0C 13 C7 08 "
+                        + appSerialNumber + " 53 07 " + startupInformation + "9000"),
+                null);
+
+        parser = new GetDataFciRespPars(apduResponse);
+
+        /* valid Calypso FCI */
+        Assert.assertTrue(parser.isValidCalypsoFCI());
+        /* DF not invalidated */
+        Assert.assertFalse(parser.isDfInvalidated());
+        /* expected dfName */
+        Assert.assertArrayEquals(ByteArrayUtils.fromHex(dfName), parser.getDfName());
+        /* expected Application Serial Number */
+        Assert.assertArrayEquals(ByteArrayUtils.fromHex(appSerialNumber),
+                parser.getApplicationSerialNumber());
+        /* Buffer size indicator and value */
+        Assert.assertEquals(siBufferSizeIndicator, parser.getBufferSizeIndicator());
+        Assert.assertEquals(1217, parser.getBufferSizeValue());
+        /* Platform */
+        Assert.assertEquals(siPlatform, parser.getPlatformByte());
+        /* ApplicationType */
+        Assert.assertEquals(siApplicationType, parser.getApplicationTypeByte());
+        Assert.assertFalse(parser.isRev3_2ModeAvailable());
+        Assert.assertFalse(parser.isRatificationCommandRequired());
+        Assert.assertFalse(parser.hasCalypsoPin());
+        Assert.assertTrue(parser.hasCalypsoStoredValue());
+        /* ApplicationSubType */
+        Assert.assertEquals(siApplicationSubtype, parser.getApplicationSubtypeByte());
+        /* SoftwareIssuer */
+        Assert.assertEquals(siSoftwareIssuer, parser.getSoftwareIssuerByte());
+        /* SoftwareVersion */
+        Assert.assertEquals(siSoftwareVersion, parser.getSoftwareVersionByte());
+        /* SoftwareRevision */
+        Assert.assertEquals(siSoftwareRevision, parser.getSoftwareRevisionByte());
+    }
+
+    @Test
+    public void testFCIparser_FCI_Invalidated() {
+        String dfName = "00112233445566778899";
+        String appSerialNumber = "AABBCCDDEEFF0011";
+        byte siBufferSizeIndicator = 11;
+        byte siPlatform = (byte) 0x55;
+        byte siApplicationType = (byte) 0xAA;
+        byte siApplicationSubtype = (byte) 0x55;
+        byte siSoftwareIssuer = (byte) 0xAA;
+        byte siSoftwareVersion = (byte) 0x55;
+        byte siSoftwareRevision = (byte) 0xAA;
+        String startupInformation = String.format("%02X%02X%02X%02X%02X%02X%02X",
+                siBufferSizeIndicator, siPlatform, siApplicationType, siApplicationSubtype,
+                siSoftwareIssuer, siSoftwareVersion, siSoftwareRevision);
+        ApduResponse apduResponse = new ApduResponse(
+                ByteArrayUtils.fromHex("6F 24 84 0A " + dfName + " A5 16 BF0C 13 C7 08 "
+                        + appSerialNumber + " 53 07 " + startupInformation + "6283"),
+                null);
+
+        GetDataFciRespPars parser = new GetDataFciRespPars(apduResponse);
+
+        /* valid Calypso FCI */
+        Assert.assertTrue(parser.isValidCalypsoFCI());
+        Assert.assertTrue(parser.isDfInvalidated());
+    }
+
+
+    @Test
+    public void testFCIparser_FCI_BadTags() {
+        String dfName = "00112233445566778899";
+        String appSerialNumber = "AABBCCDDEEFF0011";
+        byte siBufferSizeIndicator = 11;
+        byte siPlatform = (byte) 0x55;
+        byte siApplicationType = (byte) 0xAA;
+        byte siApplicationSubtype = (byte) 0x55;
+        byte siSoftwareIssuer = (byte) 0xAA;
+        byte siSoftwareVersion = (byte) 0x55;
+        byte siSoftwareRevision = (byte) 0xAA;
+        String startupInformation = String.format("%02X%02X%02X%02X%02X%02X%02X",
+                siBufferSizeIndicator, siPlatform, siApplicationType, siApplicationSubtype,
+                siSoftwareIssuer, siSoftwareVersion, siSoftwareRevision);
+
+        /* bad tag FCI Template (not constructed) */
+        ApduResponse apduResponse = new ApduResponse(
+                ByteArrayUtils.fromHex("4F 24 84 0A " + dfName + " A5 16 BF0C 13 C7 08 "
+                        + appSerialNumber + " 53 07 " + startupInformation + "9000"),
+                null);
+
+        GetDataFciRespPars parser = new GetDataFciRespPars(apduResponse);
+
+        /* invalid Calypso FCI */
+        Assert.assertFalse(parser.isValidCalypsoFCI());
+
+        /* bad tag DF Name (constructed) */
+        apduResponse = new ApduResponse(
+                ByteArrayUtils.fromHex("6F 24 C4 0A " + dfName + " A5 16 BF0C 13 C7 08 "
+                        + appSerialNumber + " 53 07 " + startupInformation + "9000"),
+                null);
+
+        parser = new GetDataFciRespPars(apduResponse);
+
+        /* invalid Calypso FCI */
+        Assert.assertFalse(parser.isValidCalypsoFCI());
+
+        /* bad tag FCI Proprietary Template (not constructed) */
+        apduResponse = new ApduResponse(
+                ByteArrayUtils.fromHex("6F 24 84 0A " + dfName + " 85 16 BF0C 13 C7 08 "
+                        + appSerialNumber + " 53 07 " + startupInformation + "9000"),
+                null);
+
+        parser = new GetDataFciRespPars(apduResponse);
+
+        /* invalid Calypso FCI */
+        Assert.assertFalse(parser.isValidCalypsoFCI());
+
+        /* bad tag FCI Issuer Discretionary */
+        apduResponse = new ApduResponse(
+                ByteArrayUtils.fromHex("6F 24 84 0A " + dfName + " A5 16 BF0D 13 C7 08 "
+                        + appSerialNumber + " 53 07 " + startupInformation + "9000"),
+                null);
+
+        parser = new GetDataFciRespPars(apduResponse);
+
+        /* invalid Calypso FCI */
+        Assert.assertFalse(parser.isValidCalypsoFCI());
+
+        /* bad tag Application Serial Number */
+        apduResponse = new ApduResponse(
+                ByteArrayUtils.fromHex("6F 24 84 0A " + dfName + " A5 16 BF0C 13 87 08 "
+                        + appSerialNumber + " 53 07 " + startupInformation + "9000"),
+                null);
+
+        parser = new GetDataFciRespPars(apduResponse);
+
+        /* invalid Calypso FCI */
+        Assert.assertFalse(parser.isValidCalypsoFCI());
+
+        /* bad tag Discretionary Data */
+        apduResponse = new ApduResponse(
+                ByteArrayUtils.fromHex("6F 24 84 0A " + dfName + " A5 16 BF0C 13 C7 08 "
+                        + appSerialNumber + " 43 07 " + startupInformation + "9000"),
+                null);
+
+        parser = new GetDataFciRespPars(apduResponse);
+
+        /* invalid Calypso FCI */
+        Assert.assertFalse(parser.isValidCalypsoFCI());
     }
 }
