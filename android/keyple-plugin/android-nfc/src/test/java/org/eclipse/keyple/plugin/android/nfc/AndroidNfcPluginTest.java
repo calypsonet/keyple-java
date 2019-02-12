47ed511104cd342098117c07c58d7dfb422b3025
OlivierDelcroix <odelcroi@gmail.com>
2019-02-12 11:46:04 +0100

    store gradle dependencies in gradle.properties

--- android/keyple-plugin/android-nfc/src/test/java/org/eclipse/keyple/plugin/android/nfc/AndroidNfcPluginTest.java
+++ android/keyple-plugin/android-nfc/src/test/java/org/eclipse/keyple/plugin/android/nfc/AndroidNfcPluginTest.java
@@ -14,7 +14,6 @@ package org.eclipse.keyple.plugin.android.nfc;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertThat;
-import static org.powermock.api.mockito.PowerMockito.when;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,7 +22,6 @@ import org.eclipse.keyple.seproxy.exception.KeypleReaderException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import junit.framework.Assert;
